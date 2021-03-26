e255334c0f1559b7946efe9d49cf1ee96d449173
Yiu <wmy@live.hk>
2021-03-26 10:15:51 +0800

    update version number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         dmmFilter
 // @namespace    https://vazue.com/
-// @version      1.0.0
+// @version      1.1.0
 // @description
 // @author       Man Yiu
 // @match        https://www.dmm.co.jp/digital/videoa/-/list/*
